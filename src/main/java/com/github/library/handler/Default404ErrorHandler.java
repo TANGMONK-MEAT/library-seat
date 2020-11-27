77ac67a2a5ada7d65c81833cb776fd0cc353da6d
zwl <2950371251@qq.com>
2020-11-27 10:37:51 +0800

    添加处理404错误的handler

---
+++ src/main/java/com/github/library/handler/Default404ErrorHandler.java
@@ -0,0 +1,40 @@
+package com.github.library.handler;
+
+import com.github.library.annotation.ResultResponseBody;
+import com.github.library.result.Result;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 处理 404 错误
+ * @author zwl
+ * @date 2020/11/27 10:32
+ */
+@Controller
+@ResultResponseBody
+public class Default404ErrorHandler implements ErrorController {
+
+
+    private final static String ERROR_PATH = "/error";
+
+    /**
+     * 处理　404 错误
+     */
+    @RequestMapping(value = ERROR_PATH)
+    public Result<Object> handle404Error(HttpServletRequest request){
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if(statusCode == HttpStatus.NOT_FOUND.value()){
+            return Result.failure(HttpStatus.NOT_FOUND);
+        }
+        return null;
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+}
